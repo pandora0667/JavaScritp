f5640967fe8f8d83240e784a3afdeb8faf336725
jusk2 <jusk2@naver.com>
2016-12-27 00:06:23 +0900

    webServer

---
+++ Start/webserver.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jusk2 on 2016-12-26.
+ */
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1337;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.end('Hello World\n');
+
+}).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
